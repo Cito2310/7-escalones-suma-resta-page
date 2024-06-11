1d7faa7eb9daa84139add25a865e9d8047324d99
Cito2310 <cito2310@gmail.com>
2024-06-11 01:11:23 -0300

    finish project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ShowNumber } from "./ShowNumber"
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-[#1F1F1F]">
+        <ShowNumber/>
+    </div>
+  )
+}
+
+export default App
